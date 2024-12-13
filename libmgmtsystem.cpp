202fa4d3c5ed148c96de57860904fef079b4bb2e
AaravSaklani <114976951+GriffynPython@users.noreply.github.com>
2024-12-13 18:11:19 +0530

    Update libmgmtsystem.cpp
    Adding comments to explain code and decisions.

--- libmgmtsystem.cpp
+++ libmgmtsystem.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-
+//This is a struct dat structure for a book.It contains a book's information. 
 struct Library{
   int price;
   string ISBN;
@@ -12,9 +12,13 @@ struct Library{
   bool Available;
   int borrow_time;
 };
-
+//This is a vector created to store a book struct data structure. It is a collection of books(struct).
+//It is also called a Library.
 vector<Library> lib;
-
+//A function created to return a book.
+//I made this to divide the management system into smaller tasks like this function.
+//Also to help me practice functions in a big(moderate) program.
+//This function is used to return a borrowed book.
 void bookReturn(string s){
   bool found=false;
   for(auto& book:lib){
@@ -56,7 +60,8 @@ void bookReturn(string s){
     cout<<"S does not match any known books, ISBN or author name."<<endl;
   }
 }
-
+//This function is used to borrow a book from a Library(vector).
+//It has a string parameter whiich it uses to find out the book the user wants to borrow.
 void checkOut(string s){
   int z;
   bool found=false;
@@ -108,7 +113,8 @@ void checkOut(string s){
     cout<<"S does not match any known books, ISBN or author name."<<endl;
   }
 }
-
+//It is a function used to search for a book in a Library.
+//The user uses this function to find a book he likes or has an interest in borrowing.
 void bookSearch(string search){
   bool found=false;
   for(auto& book:lib){
@@ -143,13 +149,14 @@ void bookSearch(string search){
       break;
     }
   }
+  //Found is a good flag. It is used in all the functions.
   if(!found){
     cout<<"S does not match any known books, ISBN or author name."<<endl;
   }
 }
 
 int main(){
-
+  //This code pushes information about books into the Library.
   lib.push_back({499,"187987","Harry Potter 1","J.K Rowling",true,0});
   lib.push_back({499,"187988","Harry Potter 2","J.K Rowling",true,0});
   lib.push_back({499,"187989","Harry Potter 3","J.K Rowling",true,0});
@@ -170,7 +177,7 @@ int main(){
 
   cout<<"Welcome to the Library."<<endl;
   cout<<"The books available in the library are:"<<endl;
-  //Display information regarding all the books present in the library
+  //Display information regarding all the books present in the library including its availability and borrow time.
   for (const auto& book : lib) {
     cout << " - " << book.title
          << " | Borrow Time: " << (book.Available ? "N/A" : to_string(book.borrow_time))
@@ -189,23 +196,29 @@ int main(){
       cout<<" (Not Available)"<<endl;
     }
   }*/
-  int x,y,ct,Fine=10;
+  //Fine is declared and initialized for calculation.
+  int x=1,y,ct,Fine=10;
   string str;
   while(x!=0){
+    //Options that the user can do. 
     cout<<"Press 1 to borrow a book"<<endl;
     cout<<"Press 2 to return the book"<<endl;
     cout<<"Press 3 to search for a book"<<endl;
     cout<<"What do you want to do? Enter a number: ";
     cin>>y;
+    //I used cin.ignore here to clear out the input buffer.
+    cin.ignore();
     if(y==1){
       cout<<"Tell me which book you want to borrow: ";
-      cin>>str;
+      //I used the getline function with the cin here to include multi word inputs as cin only handles one word.
+      getline(cin,str);
       checkOut(str);
     }
     else if(y==2){
       cout<<"Tell me which book you want to return:"<<endl;
-      cin>>str;
+      getline(cin,str);
       for (auto& book : lib) {
+        //Traverse throguh every book struct data structure, check if the user input matches and the book is available and calculate its borrow time.
             if ((str == book.title || str == book.ISBN || str == book.author) && !book.Available) {
                 if (book.borrow_time < 0) {
                     int oD = abs(book.borrow_time);
@@ -214,15 +227,18 @@ int main(){
                 break;
             }
         }
+      //This function returns the book.
       bookReturn(str);
     }
     else if(y==3){
       cout<<"Tell me which book you want to search for: ";
-      cin>>str;
+      getline(cin,str);
+      //This function searches the book by taking in the user input as an argument.
       bookSearch(str);
     }
     cout<<"Enter a value other than 0 to keep the library going"<<endl;
     cout<<"Enter 0 to close the Library: ";
+    //This is a flag variable used to control the while loop.
     cin>>x;
     
   }
