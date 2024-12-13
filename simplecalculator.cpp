d7fe0bf4e4412e92af672407bd434fea8cef5323
AaravSaklani <114976951+GriffynPython@users.noreply.github.com>
2024-12-13 18:35:35 +0530

    Update simplecalculator.cpp
    Added comments.

--- simplecalculator.cpp
+++ simplecalculator.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+//I used the functions here to practice how to use them in a program effectively.
+//They also help in increasing my programming skill and is good practice.
 int add(int a,int b){
   return a+b;
 }
@@ -15,6 +16,7 @@ int mul(int a,int b){
 }
 
 int div1(int a,int b){
+  //Error checking. Divide by zero
   if(b==0){
     cout<<"Invalid input.Divide by zero";
   }
@@ -30,21 +32,23 @@ int main(){
   string op;
   cout<<"Enter type of operation:+,-,*,/"<<" ";
   cin>>op;
+  //Could have also used the switch case statement here but I like using if-else statements more.
+  //They are much simpler than the switch case statements.
   if(op=="+"){
     result=add(x,y);
-    cout<<"The addition between two numbers"<<x<<"and"<<y<<"is:"<<result<<endl;
+    cout<<"The addition between two numbers "<<x<<" and "<<y<<" is: "<<result<<endl;
   }
   else if(op=="-"){
     result=sub(x,y);
-    cout<<"The subtraction between two numbers"<<x<<"and"<<y<<"is:"<<result<<endl;
+    cout<<"The subtraction between two numbers "<<x<<" and "<<y<<" is: "<<result<<endl;
   }
   else if(op=="*"){
     result=mul(x,y);
-    cout<<"The multiplication between two numbers"<<x<<"and"<<y<<"is:"<<result<<endl;
+    cout<<"The multiplication between two numbers "<<x<<" and "<<y<<" is: "<<result<<endl;
   }
   else if(op=="/"){
     result=div1(x,y);
-    cout<<"The division between two numbers"<<" "<<x<<"and"<<y<<"is:"<<result<<endl;
+    cout<<"The division between two numbers"<<" "<<x<<" and "<<y<<" is: "<<result<<endl;
   }
   else{
     cout<<"Invalid input."<<endl;
