8d250780c66dd6ee99202b14a47781a7653098bc
AaravSaklani <114976951+GriffynPython@users.noreply.github.com>
2024-12-13 18:15:21 +0530

    Update numbergenerator.cpp
    Added comments to help understand code and decisions.

--- numbergenerator.cpp
+++ numbergenerator.cpp
@@ -4,20 +4,32 @@
 using namespace std;
 
 int main(){
+ //I used the range of [1-10] as it would be easier to guess the random number 
+// and be less time consuming
  srand(static_cast<unsigned>(std::time(0)));
-
+//random number is stored in a variable.
  int randomNumber = std::rand() % 10 + 1;
  int guess_no;
  while(guess_no!=randomNumber){
   cout<<"Enter your guess:"<<endl;
   cin>>guess_no;
-  if(guess_no>randomNumber){
-    cout<<"Too High"<<endl;
+  //If guess no is higher than randomnumber more than 1.
+  if(guess_no-randomNumber>1){
+    cout<<"Too High! Guess lower"<<endl;
   }
-  else{
-    cout<<"Too Low"<<endl;
+   //If guess no is higher than random number by 1.
+  else if(guess_no-randomNumber==1){
+    cout<<"A little lower"<<endl;
+  }
+   //If guess no is lower than random number more than 1.
+  else if(randomNumber-guess_no>1){
+    cout<<"Guess Higher"<<endl;
+  }
+   //If guess no is lower than random number by 1.
+  else if(randomNumber-guess_no==1){
+    cout<<"Very Close. Guess higher"<<endl;
   }
  }
  cout<<"Congratulations! Your guess was right"<<endl;
- cout<<"The random number was:"<<randomNumber<<endl; 
+ cout<<"The random number was: "<<randomNumber<<endl; 
 }
